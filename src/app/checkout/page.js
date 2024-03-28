5a9c6fee0f64eeb2840533dbf1127c1bc5b4da59
DamienDrozd <damien.drozd@icloud.com>
2024-03-28 14:43:18 +0100

    feature(frontend): stripe

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -2,14 +2,19 @@
 import { checkout } from '@/services/api/order';
 import TextField from '@mui/material/TextField'
 import { useState } from 'react';
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { getStripe } from '@/lib/stripe_utils';
+
 
 const Page = () => {
+    const stripe = getStripe();
+    const elements = useElements();
 
     const [userForm, setUserForm] = useState({
         delivery_mode: "",
         delivery_address: "",
         delivery_city: "Nanterre",
-        delivery_zipcode : 92000,
+        delivery_zipcode: 92000,
         products: [1, 3, 4, 6]
     });
 
@@ -19,28 +24,34 @@ const Page = () => {
 
     const submit = async (e) => {
         e.preventDefault();
+        const cardElement = elements?.getElement("card");
+
         checkout(userForm)
-        .then(async (res) => {
-            console.log(res)
-        })
+            .then(async (res) => {
+                clientSecret = res.data.stripe_payment_id;
+                await stripe?.confirmCardPayment(clientSecret, {
+                    payment_method: { card: cardElement },
+                });
+            })
     };
 
-    return(
+    return (
         <div className="min-h-screen mx-auto flex flex-col items-center">
             <form className='w-2/5' onSubmit={(e) => submit(e)}>
+                <CardElement />
                 <h2 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white">Paiement</h2>
 
                 <ul class="grid w-full gap-5 md:grid-cols-2">
                     <li>
-                        <input type="radio" name="delivery_mode" id="delivery" value="delivery" checked={userForm.delivery_mode === 'delivery'} onChange={(e)=> handleChange(e)} class="hidden peer" required />
-                        <label for="delivery" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-sm cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-black peer-checked:border-black peer-checked:text-black hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                        <input type="radio" name="delivery_mode" id="delivery" value="delivery" checked={userForm.delivery_mode === 'delivery'} onChange={(e) => handleChange(e)} class="hidden peer" required />
+                        <label for="delivery" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-sm cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-black peer-checked:border-black peer-checked:text-black hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <div class="block">
                                 <div class="w-full text-l font-semibold">Delivery</div>
                             </div>
                         </label>
                     </li>
                     <li>
-                        <input type="radio" name="delivery_mode" id="pick-up" value="pick-up"  checked={userForm.delivery_mode === 'pick-up'} onChange={(e)=> handleChange(e)} class="hidden peer"/>
+                        <input type="radio" name="delivery_mode" id="pick-up" value="pick-up" checked={userForm.delivery_mode === 'pick-up'} onChange={(e) => handleChange(e)} class="hidden peer" />
                         <label for="pick-up" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-sm cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-black peer-checked:border-black peer-checked:text-black hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                             <div class="block">
                                 <div class="w-full text-l font-semibold">Pick-Up</div>
