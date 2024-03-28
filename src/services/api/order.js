6802a97c39d18fb1a1b47fdc80ce3ebeda99df71
clement-lnr <clementlanneretonne@yahoo.fr>
2024-03-28 14:41:49 +0100

    fix(front): checkout

--- src/services/api/order.js
+++ src/services/api/order.js
@@ -2,9 +2,19 @@ import api from "./server";
 
 export async function checkout(data) {
     try {
-        const response = await api.post("/orders", data);
-        return response.data;
-    } catch (error) {
-        console.log(error)
+        let storeToken = localStorage.getItem('storeToken');
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/orders`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization" : `Bearer ${storeToken}`
+            },
+            body: JSON.stringify(data),
+        });
+        return res.json();
+    }
+    catch (err) {
+        return err;
     }
 }
